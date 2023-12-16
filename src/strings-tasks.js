f715e5bd4abb485f8d70e8d8d183d8d67395bb7b
wasek07 <wasek07@ya.ru>
2023-12-16 10:20:15 +0100

    feat: add solution for removes only trailing whitespace

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -110,8 +110,8 @@ function removeLeadingWhitespaces(value) {
  *   removeTrailingWhitespaces('cat ') => 'cat'
  *   removeTrailingWhitespaces('\t\t\tHello, World! ') => '\t\t\tHello, World!'
  */
-function removeTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeTrailingWhitespaces(value) {
+  return value.trimEnd();
 }
 
 /**
