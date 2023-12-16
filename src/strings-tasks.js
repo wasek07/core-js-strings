d841e50c4ecee229bcd967f68a828413e9b29f07
wasek07 <wasek07@ya.ru>
2023-12-16 09:46:44 +0100

    feat: add solution for return first character

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -65,8 +65,8 @@ function concatenateStrings(value1, value2) {
  *   getFirstChar('cat') => 'c'
  *   getFirstChar('') => ''
  */
-function getFirstChar(/* value */) {
-  throw new Error('Not implemented');
+function getFirstChar(value) {
+  return value.charAt(0);
 }
 
 /**
