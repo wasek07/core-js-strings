98dd6ff70546980ce55dd0e7bf9e7d148cbb6d4f
wasek07 <wasek07@ya.ru>
2023-12-16 09:56:06 +0100

    feat: add solution for removes whitespace

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -80,8 +80,8 @@ function getFirstChar(value) {
  *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
  *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  return value.trim();
 }
 
 /**
