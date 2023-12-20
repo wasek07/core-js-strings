5dfdb728f1e7640091923792d125defd1a331f29
wasek07 <wasek07@ya.ru>
2023-12-21 00:21:20 +0100

    feat: add solution for reverse words

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -351,8 +351,12 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const words = str.split(' ');
+  const wordsRevers = words.map((word) => word.split('').reverse().join(''));
+  const strRevers = wordsRevers.join(' ');
+
+  return strRevers;
 }
 
 /**
